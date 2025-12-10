7b0eec7f439b3c4d6bf6732a18de6f4b8d13a5f7
thibaultodor <60348757+thibaultodor@users.noreply.github.com>
2025-12-10 15:49:15 +0100

    Update TP 03.sql
    Change * -> REF,DESIGNATION (b)

--- TP3/TP 03.sql
+++ TP3/TP 03.sql
@@ -2,7 +2,7 @@
 SELECT * FROM article;
 
 -- b. Listez uniquement les références et désignations des articles de plus de 2 euros
-SELECT * FROM article WHERE PRIX>2;
+SELECT REF,DESIGNATION FROM article WHERE PRIX>2;
 
 -- c. En utilisant les opérateurs de comparaison, listez tous les articles dont le prix est compris entre 2 et 6.25 euros
 SELECT * FROM article WHERE PRIX>=2 AND PRIX<=6.25;
@@ -36,4 +36,5 @@ SELECT * FROM article WHERE ID_FOU
                             (SELECT ID FROM fournisseur WHERE NOM = 'Française d''Imports' OR NOM = 'Dubois & Fils');
 
 -- i. Listez tous les bons de commande dont la date de commande est entre le 01/02/2019 et le 30/04/2019.
+
 SELECT * FROM bon WHERE DATE_CMDE BETWEEN '2019-02-01' AND '2019-04-30'
